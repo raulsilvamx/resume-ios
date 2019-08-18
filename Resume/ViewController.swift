20cdf3f046b43ef62b2eb3d896e12dffdefabce0
Raul Silva <ignacio.silva@dextratech.com>
2019-08-17 21:17:21 -0500

    Initial Commit

---
+++ Resume/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resume
+//
+//  Created by Ignacio Raul Silva on 8/17/19.
+//  Copyright © 2019 SIRDISoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
